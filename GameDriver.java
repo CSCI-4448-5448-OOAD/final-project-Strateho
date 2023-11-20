4a290fc8a832df7a3de03f3c218140db1aa3eeb9
Emma Worthington <emma@virtualworthington.com>
2023-11-20 15:16:06 -0500

    logger

--- GameDriver.java
+++ GameDriver.java
@@ -1,6 +1,15 @@
 class GameDriver {
     public static void main(String[] args) {
         GameDriver game = new GameDriver();
+        while(true){
+            // Logger logger = Logger.getInstance(game.turn);
+            // game.addObserver(logger);
 
+            // game.playRound();
+            // game.turn++;
+
+            // logger.closeLogFile();
+            // game.removeObserver(logger);
+        }
     }
 }
