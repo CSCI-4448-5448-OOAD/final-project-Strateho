98dfb7ad092aa9ea57a53460d4ef52af6795843c
Emma Worthington <emma@virtualworthington.com>
2023-11-25 13:43:04 -0500

    messing w game driver

--- GameDriver.java
+++ GameDriver.java
@@ -1,19 +1,41 @@
+import java.util.*;
+
 class GameDriver {
+    int turn = 0;
+    private List<Observer> observers = new ArrayList<>();
+
     public static void main(String[] args) {
         GameDriver game = new GameDriver();
         while(true){
-            // Logger logger = Logger.getInstance(game.turn);
-            // game.addObserver(logger);
+            Logger logger = Logger.getInstance(game.turn);
+            game.addObserver(logger);
 
             // game.playRound();
-            // game.turn++;
+            game.turn++;
 
-            // logger.closeLogFile();
-            // game.removeObserver(logger);
+            game.turn++;
+
+            logger.closeLogFile();
+            game.removeObserver(logger);
         }
     }
 
     public void setup(int numPlayers){
         Board b = new Board(numPlayers);
     }
+    
+    public void addObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    public void removeObserver(Observer observer) {
+        this.observers.remove(observer);
+    }
+    
+    // @Override
+    // public void notifyObservers(Event event) {
+    //     for (Observer observer : observers) {
+    //         observer.update(event);
+    //     }
+    // }
 }
